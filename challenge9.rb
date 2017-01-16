53e3c4a481a2420a28399751e6283bdbb18cb905
Matthew Slane <mcslane@iastate.edu>
2017-01-16 17:32:49 -0600

    finished challenge 9

---
+++ challenge9.rb
@@ -0,0 +1,18 @@
+
+module Challenge9
+  def self.pcks7_padding(str, n)
+    c = n - str.size
+    for i in (0...c) do
+      str += c.chr
+    end
+
+    str
+  end
+end
+
+if __FILE__ == $0
+  str = "YELLOW SUBMARINE"
+  padded = Challenge9::pcks7_padding(str, 20)
+  puts padded
+  p padded.each_byte.to_a
+end
